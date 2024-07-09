7ad623cac04631695c3eca5dbd95792829c87ff1
yajuvendrashah <103363278+yajuvendrashah@users.noreply.github.com>
2024-07-09 12:42:35 +0530

    Update tictactoe.jsx

--- tictactoe.jsx
+++ tictactoe.jsx
@@ -4,9 +4,9 @@ import './App.css';
 const initialBoard = Array(9).fill(null);
 
 const winLines = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-  [0, 4, 8], [2, 4, 6]             // Diagonals
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+  [0, 4, 8], [2, 4, 6]             
 ];
 
 const App = () => {
